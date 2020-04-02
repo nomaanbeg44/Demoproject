b6b867fde3ff7c386d0e958b838522e1324488a2
nomaanbeg44 <51062512+nomaanbeg44@users.noreply.github.com>
2020-04-01 18:25:33 -0700

    template changes

--- DEMOPROJECTS/amazonReviewsAnalysis/models.py
+++ DEMOPROJECTS/amazonReviewsAnalysis/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import pandas as pd
@@ -14,28 +14,29 @@ class Reviews:
     comment:str
     email:str
     response:str
+    reviews_count=[]
     def modelFit(self):
         data = pd.read_csv("Reviews.csv")
-        df = data.loc[:, ['Summary', 'Score', 'HelpfulnessNumerator', 'HelpfulnessDenominator']]
-        df.dropna(subset=['Summary'], inplace=True)
-        df = df[df['Score'] != 3]
-        df["Sentiment"] = df["Score"].apply(lambda score: "positive" if score > 3 else "negative")
-        df["Summary_Clean"] = df["Summary"].apply(self.cleanup)
-        vectorizer = CountVectorizer(ngram_range=(1, 2))
-        reviews_tfidf = vectorizer.fit_transform(df['Summary_Clean'])
+        data = data.loc[:, ['Summary','Text', 'Score', 'ProductId','HelpfulnessNumerator','UserId', 'HelpfulnessDenominator']]
+        data["Sentiment"] = data["Score"].apply(lambda score: "positive" if score > 3 else "negative")
+        data['Helpfulness'] = data['HelpfulnessNumerator']/data['HelpfulnessDenominator']
+        #data.dropna(subset=['Summary'], inplace=True)
+        data['Text_Clean']=data['Text'].apply(self.cleanup)
+        df=data[data['Score']!=3]
+        tfidf_transformer = TfidfVectorizer(ngram_range=(1,2), stop_words='english')
+        reviews_tfidf = tfidf_transformer.fit_transform(df['Text_Clean'])
         X = reviews_tfidf
         y = df['Sentiment']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
         logmodel = LogisticRegression()
         model_fit = logmodel.fit(X_train, y_train)
-        returnValue = [model_fit,vectorizer,df]
+        returnValue = [model_fit,tfidf_transformer,data]
         return returnValue
 
     def cleanup(self,sentence):
         cleanup_re = re.compile('[^a-z]+')
         sentence = sentence.lower()
         sentence = cleanup_re.sub(' ', sentence).strip()
-        # sentence = " ".join(nltk.word_tokenize(sentence))
         return sentence
 
 
--- DEMOPROJECTS/amazonReviewsAnalysis/views.py
+++ DEMOPROJECTS/amazonReviewsAnalysis/views.py
@@ -28,20 +28,24 @@ def homepage(request):
     return render(request, 'homepage.html')
 
 def visualization(request):
+    data=k[2]
+    genuine_reviews= round((len(data[ data['Helpfulness']>.50 ])/len(data)),2)
+    revs.reviews_count=[round((len(data[ (data['Score']==4) | (data['Score']==5) ])/len(data)),2),
+                       round((len(data[ (data['Score']==1) | (data['Score']==2) ])/len(data)),2),
+                       round((len(data[data['Score']==3])/len(data)),2),
+                       genuine_reviews ]
     email=request.POST["email"]
     password=request.POST["pass"]
     if email=='a@a.com' and password=='admin':
-        return render(request, 'adminPage.html')
+        return render(request, 'adminPage.html',{'loader': revs})
     else:
         return render(request, 'login.html')
-    #return render(request, 'visualizationPage.html') if email=='admin@amazon.com' and password=='amazon@123' else return render(request, 'homepage.html')
 
-def getfigure(request):
+def getPlot1(request):
     data=k[2]
-    group = data.groupby('Sentiment')['Summary'].count()
+    group = data.groupby('Sentiment')['Text'].count()
     col=['Negative', 'Positive']
     count=[group.negative, group.positive]
-    output_file("reviewCount.html")
     p = figure(x_range=col, plot_height=250, title="Total positive and Negative Reviews",
                toolbar_location=None, tools="")
     p.vbar(x=col, top=count, width=0.9)
@@ -49,10 +53,55 @@ def getfigure(request):
     p.xgrid.grid_line_color = None
     p.y_range.start = 0
     script, div =components(p)
-    #print(script)
-    #return render(request, 'visualizationPage.html', {'script':script, 'div':div})
     show(p)
-    return render(request, 'adminPage.html')
+    return render(request, 'adminPage.html',{'loader': revs})
+
+def getPlot3(request):
+    data=k[2]
+    group = data.groupby('Score')['Text'].count().rename_axis('Score').reset_index(name='Total_Reviews')
+    col=['1','2','3','4','5']
+    count=[group.Total_Reviews[0],group.Total_Reviews[1],group.Total_Reviews[2],group.Total_Reviews[3],group.Total_Reviews[4]]
+    p = figure(x_range=col, plot_height=250, title="Total Reviews for evey Score",
+           toolbar_location=None, tools="")
+    p.vbar(x=col, top=count, width=0.9)
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+    show(p)
+    return render(request, 'adminPage.html',{'loader': revs})
+
+def getPlot4(request):
+    data=k[2]
+    top_users = data['UserId'].value_counts().head(20).rename_axis('UserId').reset_index(name='Total_Reviews')
+    i3 = top_users.set_index('UserId').index
+    i4 = data.set_index('UserId').index
+    top_users = pd.merge(top_users,
+                         data[i4.isin(i3)].groupby('UserId')['Helpfulness'].mean().rename_axis('UserId').reset_index(name='Avg_Helpfulness_percentage'),
+                         on="UserId")
+    p = figure(x_range=top_users['UserId'].tolist(), plot_height=400, plot_width=700, title="Top 20 users with Maximum numbers of reviews given",
+               toolbar_location=None,y_range=(0, 500))
+    p.vbar(x=top_users['UserId'].tolist(), top=top_users['Total_Reviews'].tolist(), width=0.5)
+    p.xaxis.major_label_orientation = "vertical"
+    p.y_range.start = 0
+    show(p)
+    return render(request, 'adminPage.html',{'loader': revs})
+
+def getPlot2(request):
+    review_df=k[2]
+    top_products = review_df['ProductId'].value_counts().head(20).rename_axis('Product_Id').reset_index(name='Total Reviews')
+    i1 = review_df.set_index('ProductId').index
+    i2 = top_products.set_index('Product_Id').index
+    top_products = pd.merge(top_products,
+                            review_df[i1.isin(i2)].groupby('ProductId')['Score'].mean().rename_axis('Product_Id').reset_index(name='Avg_score'),
+                            on="Product_Id")
+    output_file("topProduct.html")
+    p = figure(x_range=top_products['Product_Id'].tolist(), plot_height=400, plot_width=700, title="Top Reviewed products",
+               toolbar_location=None,y_range=(0, 1000))
+
+    p.vbar(x=top_products['Product_Id'].tolist(), top=top_products['Total Reviews'].tolist(), width=0.5)
+    p.xaxis.major_label_orientation = "vertical"
+    p.y_range.start = 0
+    show(p)
+    return render(request, 'adminPage.html',{'loader': revs})
 
 
 def about(request):
